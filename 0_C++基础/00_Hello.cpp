b43f3e270ac16dc762420da4e7b6990afd52ee3a
seuleseule <soul.seule@qq.com>
2020-01-07 22:58:12 +0800

    Hello_World

---
+++ 0_C++基础/00_Hello.cpp
@@ -0,0 +1,26 @@
+/*
+#define   _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	int	ErroMsg = 0;
+	
+	
+	return ErroMsg;
+}
+*/
+#include "iostream" // 包含C++头  //iostream.h
+
+using namespace std;  //std 标准命名空间，定义了很多标准定义。
+
+void main() {
+
+	// cout 标准输出 对象？？？ 可能是黑屏幕
+	// << 左移操作符 不是左移？？ 据说是操作符重载
+	// endl == \n
+	cout << "Hello World!" << endl;
+	system("pause");
+}
