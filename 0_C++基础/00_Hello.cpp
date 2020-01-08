841109146df222361b362e2e1bcaf4592876c3c5
seuleseule <soul.seule@qq.com>
2020-01-08 16:16:42 +0800

    面向对象思想初识

--- 0_C++基础/00_Hello.cpp
+++ 0_C++基础/00_Hello.cpp
@@ -16,11 +16,60 @@ int main()
 
 using namespace std;  //std 标准命名空间，定义了很多标准定义。
 
-void main() {
+void main_00() {
 
 	// cout 标准输出 对象？？？ 可能是黑屏幕
 	// << 左移操作符 不是左移？？ 据说是操作符重载
 	// endl == \n
+	cout << "Hello World!" << endl;
+	system("pause");
+}
+
+//面向过程，圆面积计算
+void main_01() {
+	double	r = 0;
+	double	s = 0;
+	cout << "输入半径：";
+	cin >> r;
+
+	s = 3.14  *r * r;
+
+	cout << "面积：" << s << endl;
+
+	cout << "Hello World!" << endl;
+	system("pause");
+}
+
+
+//面向对象
+//内存分配？？
+//类方法参数传递？？
+class  Round
+{
+public:
+	double	m_r;
+	double	m_s;
+
+public:
+	void SetR(double r)
+	{
+		m_r = r;
+	}
+	double GetS()
+	{
+		m_s = m_r * m_r * 3.14;
+		return m_s;
+	}
+};
+
+void main() {
+	Round	c;
+	double	r;
+	cout << "输入半径：";
+	cin >> r;
+	c.SetR(r);
+
+	cout << "面积：" << c.GetS() << endl;
 	cout << "Hello World!" << endl;
 	system("pause");
 }
