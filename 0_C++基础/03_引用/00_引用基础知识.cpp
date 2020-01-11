c378ad1b2c1bd7c29fad41945baf8913d791469d
seuleseule <soul.seule@qq.com>
2020-01-12 00:15:35 +0800

    引用的基础知识及本质分析

---
+++ 0_C++基础/03_引用/00_引用基础知识.cpp
@@ -0,0 +1,107 @@
+﻿#include "iostream"
+
+using namespace std;
+
+// 引用： 已定义变量的别名
+// 语法：  TYPE& name = var;
+
+
+
+void main00() 
+{
+	int		a = 10;
+	int&	b = a;
+	// int& c; // ERRO 普通引用必须初始化
+	b = 20;
+
+	cout << a << endl;
+	cout << "Hello World!" << endl;
+	system("pause");
+}
+
+// 引用做函数参数不用初始化
+void IntSwap0(int a, int b)
+{
+	int c = 0;
+	c = a;
+	a = b;
+	b = c;
+}
+void IntSwap1(int *a,int *b)
+{
+	int c = 0;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+void IntSwap2(int& a, int& b)
+{
+	int c = 0;
+	c = a;
+	a = b;
+	b = c;
+}
+
+void main01()
+{
+	int a = 10;
+	int b = 20;
+
+	IntSwap0(a, b);
+	cout <<"a:"<< a << "  b:" << b<<  endl;
+
+	IntSwap1(&a, &b);
+	cout << "a:" << a << "  b:" << b << endl;
+
+	IntSwap2(a, b);
+	cout << "a:" << a << "  b:" << b << endl;
+
+	cout << "Hello World!" << endl;
+	system("pause");
+}
+
+// 对复杂数据类型的引用
+
+struct stud
+{
+	int age;
+	int name[20];
+};
+
+void PrintStud0(stud* s)
+{
+	printf("PrintStud0(stud* s) s.age:%d\n", s->age);
+	s->age = 21;   //指针间接修改
+}
+
+void PrintStud1(stud s)
+{
+	printf("rintStud1(stud s) s.age:%d\n", s.age);
+	s.age = 22;   // ERRO，修改形参，无效修改
+}
+
+void PrintStud2(stud &s)
+{
+	printf("PrintStud2(stud &s) s.age:%d\n", s.age);
+	s.age = 23;   // 通过引用修改参数，有效操作
+}
+
+
+void main()
+{
+	stud s1;
+	s1.age = 20;
+
+	PrintStud0(&s1);
+	printf("PrintStud0(&s1); s1.age:%d\n", s1.age);
+
+	PrintStud1(s1);
+	printf("PrintStud1(s1); s1.age:%d\n", s1.age);
+
+	PrintStud2(s1);
+	printf("PrintStud2(s1); s1.age:%d\n", s1.age);
+	
+	system("pause");
+
+}
---
+++ 0_C++基础/03_引用/01_引用的本质.cpp
@@ -0,0 +1,68 @@
+﻿#include "iostream"
+
+using namespace std;
+
+
+// 引用必须初始化 // 类似 const
+// 引用变量地址与原变量一致，是一个指针？
+// 
+// 引用本质： TYPE * cosnt var;
+void main00() 
+{
+	int const a = 10;
+	int b = 20;
+	int& c = b;
+
+	printf("&b:%d\n", &b);
+	printf("&c:%d\n", &c);  // 一样的值
+
+
+	cout << "Hello World!" << endl;
+	system("pause");
+}
+
+// 引用占用空间
+struct stud
+{
+	char name[20];  // 20字节
+	int age;  // 4字节
+
+	int& a;  // 4字节
+	int& b;  // 4字节
+};
+
+void main01()
+{
+	printf("sizeof(stud):%d\n", sizeof(stud));
+
+
+	system("pause");
+}
+
+
+
+// 间接赋值 与引用 ？？？
+void change0(int* const a)
+{
+	*a = 1000;
+}
+
+void change1(int& a)
+{
+	a = 2000;
+}
+
+void main()
+{
+	int a = 10;
+
+	printf("a:%d\n", a);
+
+	change0(&a);  // 间接赋值
+	printf("a:%d\n", a);
+	 
+	change1(a);
+	printf("a:%d\n", a); // 引用  
+
+	system("pause");
+}
