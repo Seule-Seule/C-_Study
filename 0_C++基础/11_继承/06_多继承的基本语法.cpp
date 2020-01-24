26a9bb4bab6e141aa078eb7e021dffd62f866c9c
seuleseule <soul.seule@qq.com>
2020-01-24 13:32:38 +0800

    多继承的二义性和虚继承

---
+++ 0_C++基础/11_继承/06_多继承的基本语法.cpp
@@ -0,0 +1,65 @@
+﻿
+#include "iostream"
+using namespace std;
+
+class Test1
+{
+public:
+	Test1(int t)
+	{
+		this->t1 = t;
+	}
+public:
+	void PrintT1()
+	{
+		cout << " t1; " << t1 << endl;
+	}
+private:
+	int t1;
+};
+
+
+class Test2
+{
+public:
+	Test2(int t)
+	{
+		this->t2 = t;
+	}
+public:
+	void PrintT2()
+	{
+		cout << " t1; " << t2 << endl;
+	}
+private:
+	int t2;
+};
+
+
+class Test:public Test1, public Test2
+{
+public:
+	Test(int t1, int t2, int t3):Test1(t1),Test2(t2)
+	{
+		this->t3 = t3;
+	}
+public:
+	void PrintT3()
+	{
+		cout << " t1; " << t3 << endl;
+	}
+private:
+	int t3;
+};
+int main() 
+{
+	int ret = 0;
+
+	Test t1(1, 2, 3);
+	t1.PrintT1();
+	t1.PrintT2();
+	t1.PrintT3();
+	cout << "Hello World!" << endl;
+	system("pause");
+	return ret;
+}
---
+++ 0_C++基础/11_继承/07_二义性和虚函数.cpp
@@ -0,0 +1,58 @@
+﻿
+#include "iostream"
+using namespace std;
+
+
+
+//..........Test1.........
+//........./.....\........
+//......Tset3...Test2.....
+//.........\..../.........
+//...........Test.........
+
+
+//virtual 虚继承
+
+
+class Test1
+{
+public:
+	int b;
+};
+
+class Test2:virtual public Test1
+{
+public:
+	int b2;
+};
+
+
+class Test3:virtual public Test1
+{
+public:
+	int b3;
+};
+
+class Test :public Test2, public Test3
+{
+public:
+	int bt;
+};
+
+
+
+int main() 
+{
+	int ret = 0;
+
+	Test t1;
+	t1.b2 = 2; 
+	t1.b3 = 3;
+	t1.bt = 4; 
+
+	t1.b = 10; // 未加 virtual 时访问不明确
+
+	cout << "Hello World!" << endl;
+	system("pause");
+	return ret;
+}
