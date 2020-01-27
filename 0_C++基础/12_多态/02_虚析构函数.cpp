464456dab2df94631acc0698f5e104946454ab73
seuleseule <soul.seule@qq.com>
2020-01-27 20:37:01 +0800

    重载、重写、重定义

---
+++ 0_C++基础/12_多态/02_虚析构函数.cpp
@@ -0,0 +1,74 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include "iostream"
+using namespace std;
+
+class TestA
+{
+public:
+	TestA()
+	{
+		p = new char[20];
+		strcpy(p, "a a a a a");
+	}
+	virtual ~TestA()
+	{
+		delete[]p;
+	}
+private:
+	char* p;
+};
+
+class TestB:public TestA
+{
+public:
+	TestB()
+	{
+		p = new char[20];
+		strcpy(p, "a a a a a");
+	}
+	virtual ~TestB()
+	{
+		delete[]p;
+	}
+private:
+	char* p;
+};
+
+class TestC:public TestB
+{
+public:
+	TestC()
+	{
+		p = new char[20];
+		strcpy(p, "a a a a a");
+	}
+	virtual ~TestC()
+	{
+		delete[]p;
+	}
+private:
+	char* p;
+};
+
+void Test(TestA* base)
+{
+	delete base;
+}
+
+
+// 不使用 virtual 调用父类释放函数，只释放父类资源
+// 使用 virtual 调用父类释放函数，使父类指针释放所有子类资源
+
+int main()
+{
+	int ret = 0;
+
+	TestC* pc1 = new TestC;
+	
+	Test(pc1);      
+	//delete pc1;  // 直接释放，依次调用所有父类析构函数释放资源
+	cout << "Hello World!" << endl;
+	system("pause");
+	return ret;
+}
---
+++ 0_C++基础/12_多态/03_重载a重写n重定义d.cpp
@@ -0,0 +1,65 @@
+﻿
+#include "iostream"
+using namespace std;
+
+
+class TestA
+{
+public:
+	void func()
+	{
+		cout << "TestA::func()" << endl;
+	}
+	void func(int a)
+	{
+		cout << "TestA::func(int a)" << endl;
+	}
+	void func(int a, int b)
+	{
+		cout << "TestA::func(int a, int b)" << endl;
+	}
+	virtual void func(int a, int b,int c)
+	{
+		cout << "TestA::func(int a, int b,int c)" << endl;
+	}
+};
+
+class TestB:public TestA
+{
+public:
+	void func(int a, int b, int c)
+	{
+		cout << "TestB::func(int a, int b,int c)" << endl;
+	}
+	void func(int a, int b, int c,int d)
+	{
+		cout << "TestB::func(int a, int b, int c,int d)" << endl;
+	}
+};
+
+
+// 函数重载
+//     在同一个类中的同名函数
+//     子类无法重载父类函数，父类中同名函数将被覆盖(可以通过域作用符调用)
+//     重载是由编译器编译过程中静态的由参数确定
+
+// 函数重写
+//     子类与父类中的同名函数
+//     子类与父类中的函数必须具有相同的原型
+
+
+// 函数重写分两类
+//     写了virtual的函数为虚函数重写，发生多态
+//     没写virtual的函数为函数重定义，调用父类
+
+int main() 
+{
+	int ret = 0;
+
+	TestB b1;
+	b1.TestA::func();
+
+	cout << "Hello World!" << endl;
+	system("pause");
+	return ret;
+}
