2808eeb43dbb68df80442e014fefdec4eb5ecf11
seuleseule <soul.seule@qq.com>
2020-02-15 18:10:51 +0800

    栈解旋unwimding

---
+++ 1_C++进阶/02_异常处理机制/01_栈解旋unwimding.cpp
@@ -0,0 +1,57 @@
+
+#include "iostream"
+using namespace std;
+
+class Test
+{
+public:
+	Test(int a)
+	{
+		this->a = a;
+		cout << "\t构造函数" << endl;
+	}
+	~Test()
+	{
+		cout << "\t析构函数" << endl;
+	}
+protected:
+private:
+	int a;
+};
+
+void voidTest()
+{
+	Test t1(1), t2(2), t3(3);
+	cout << "发生异常" << endl;
+	throw 2;
+}
+
+void voidTest2()
+{
+	Test t1(1), t2(2), t3(3);
+	voidTest();
+}
+
+
+// 栈解旋
+//   从进入try语句到发生异常的语句之前所有栈变量都将被析构，析构顺序与构造顺序相反
+int main() 
+{
+	int ret = 0;
+	try
+	{
+		voidTest2();
+	}
+	catch (int e)
+	{
+		cout << "捕获异常： " << e << endl;
+	}
+	catch (...)
+	{
+		cout << "未知异常： " << endl;
+	}
+
+	cout << "Hello World!" << endl;
+	system("pause");
+	return ret;
+}
